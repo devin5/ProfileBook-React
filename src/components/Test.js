afd11a7d2fb939d60b10601d7e27709d992f03da
Devin Dias <devindias.tech@gmail.com>
2020-03-31 12:09:54 -0500

    sets up ant design
    overides webpack, overrides babel, changes scripts

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "antd";
 
 const Test = () => {
-  return <h1>TEST</h1>;
+  return (
+    <div className="bDiv">
+      <h1>TEST</h1>
+      <Button type="primary">Test1</Button>
+    </div>
+  );
 };
 
 export default Test;

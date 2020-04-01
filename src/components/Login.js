20bfbdf8927afea36200d745a7551804ef94bde2
Minakshi Verma <mvedwan@gmail.com>
2020-04-01 11:42:29 -0400

    login css modified

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, {useState} from "react"
 import {connect} from 'react-redux'
 import {signIn} from '../actions'
 import { Layout, Input, Button } from 'antd'
-const {header} = Layout
+
 
 
 
